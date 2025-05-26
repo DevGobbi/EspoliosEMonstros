6961aadc4e6e3063e06676cbc5216bf57f0ccf3f
JOAO VICTOR GOBBI BUENO GONCALVES <CG3040151@car.ifsp>
2025-05-26 20:43:59 -0300

    receba

---
+++ Desktop/tigrinho/Metrosporsec.py
@@ -0,0 +1,6 @@
+distanciapercorrida = float(input('Insira a distância total percorrida pelo objeto em quilômetros!'))
+tempototal = float(input('Insira o tempo total em que o objeto se locomoveu em horas!'))
+velocidademedia = distanciapercorrida/tempototal
+velocidadefinal= round((velocidademedia/3.6))
+print (f'Sua velocidade em quilômetros por hora é equivalente a {velocidademedia}km/h!') 
+print (f'Portanto, ao converter o valor da velocidade média pra metros por segundos, você recebe o valor final de {velocidadefinal} metros por segundo!')
