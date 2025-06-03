62a56efc5597799bfcc294ae6e0e3ea9f8b0c394
João Gobbi <joaogobbibg@gmail.com>
2025-06-02 21:35:08 -0300

    Delete Desktop/tigrinho directory

--- Desktop/tigrinho/Areadeconvivencia.py
+++
@@ -1,4 +0,0 @@
-raio = float(input('Insira aqui o raio da escola:'))
-pi = 3.14
-areadocirculo = pi*raio**2
-print(f'A área total de convivência da sua escola é {areadocirculo} metros!')
--- Desktop/tigrinho/VelocidadeMedia.py
+++
@@ -1,4 +0,0 @@
-deslocamento = float(input('Insira o valor do deslocamento em metros:'))
-tempo = float(input('Insira o valor do tempo em segundos'))
-velocidademedia = deslocamento/tempo
-print (f'A velocidade total do objeto é: {velocidademedia} metros por segundo!')
