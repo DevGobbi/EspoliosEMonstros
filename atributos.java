b1209fea3b377c7bf0bf7e55ac2c528a864589f2
JOAO VICTOR GOBBI BUENO GONCALVES <CG3040151@car.ifsp>
2025-06-03 21:59:41 -0300

    Receba

--- atributos.java
+++
@@ -1,8 +0,0 @@
-public class atributos {
-     public Integer força;
-     public Integer destreza;
-     public Integer constituição;
-     public Integer sabedoria;
-     public Integer intelecto;
-     public Integer carisma;
-}
--- espoliosemonstros.java
+++ espoliosemonstros.java
@@ -1,6 +1,6 @@
-public class espoliosemonstros
-    import java.util.Scanner;
-    public static void main (String[]args) {
+import java.util.Scanner;
+public class espoliosemonstros {
+    public static void main (String[]args){
     Scanner resposta = new Scanner(System.in); 
     Integer pontosatributos = 20;
     Integer atributojogador = Integer.parseInt(resposta.nextLine());
@@ -8,9 +8,9 @@ public class espoliosemonstros
     Integer destreza = 0;
     Integer constituição = 0;
     Integer sabedoria = 0;
-    Integer intelecto = 0;
-    Integer carisma = 0;
-    //Ficha do Jogador
+    Integer intelecto = 1;
+    Integer carisma = 1;
+
     System.out.println("Qual o nome do seu personagem?");
     String nomedopersonagem = resposta.nextLine();
     
@@ -20,14 +20,34 @@ public class espoliosemonstros
     String confirmacao = resposta.nextLine();
     if (confirmacao.equals("S"))
      System.out.println("Qual o valor do atributo de força pro seu personagem? 1-5");
-    força = Integer.parseInt(resposta.nextLine());
+    força = atributojogador;
     pontosatributos = pontosatributos-força;
     System.out.print("Pontos de atributos restantes: " + pontosatributos);
     System.out.println("Qual o valor do atributo de destreza pro seu personagem? 1-5");
-    
-    
-    
-    
+    destreza = atributojogador;
+    pontosatributos = pontosatributos-destreza;
+    System.out.println("Qual o valor do atributo de constituição pro seu personagem?");
+    constituição = atributojogador;
+    pontosatributos = pontosatributos-constituição;
+    System.out.println("Qual o valor do atributo de sabedoria pro seu personagem?");
+    sabedoria = atributojogador;
+    pontosatributos = pontosatributos-sabedoria;
+    System.out.println("Qual o valor do atributo do intelecto do seu personagem?");
+    intelecto = atributojogador;
+    pontosatributos = pontosatributos-intelecto;
+    System.out.println("Qual o valor do atributo de carisma pro seu personagem?");
+    carisma = atributojogador;
+    pontosatributos = pontosatributos-carisma;
+    System.out.println("Vamos recapitular, atualmente a sua ficha está assim!");
+    System.out.println("Nome do personagem: " + nomedopersonagem);
+    System.out.println("Força:" + força);
+    System.out.println("Destreza: " + destreza);
+    System.out.println("Constituição:" + constituição);
+    System.out.println("Sabedoria: " + sabedoria);
+    System.out.println("Intelecto" + intelecto);
+    System.out.println("Carisma" + carisma);
     resposta.close();
      }
- 
+ }
+
+ // NÃO TÁ FUNCIONANDO :(
