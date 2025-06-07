30891e2a333abb8a5ad815335ee595afaaedd527
JOAO VICTOR GOBBI BUENO GONCALVES <CG3040151@car.ifsp>
2025-06-07 08:30:17 -0300

    receba

---
+++ sextafeiraplaytv.java
@@ -0,0 +1,21 @@
+public class sextafeiraplaytv {
+    public static void main(String[]args) throws InterruptedException{
+    System.out.println("Sexta feira, Play TV. Nois tá como no recolhe.");
+    Thread.sleep(2000);
+    System.out.println("Ai kraw ta mo frio,nossa");
+    Thread.sleep(1000);
+    System.out.println("Tá gostoso, não tá??");
+    Thread.sleep(1500);
+    System.out.println("Ai vem com o pix caralho");
+    Thread.sleep(2000);
+    System.out.println("Hoje nois ta como?");
+    Thread.sleep(1000);
+    System.out.println("XAMBÃO");
+    Thread.sleep(1000);
+    System.out.println("Botando pra pocar");
+    Thread.sleep(1500);
+    System.out.println("Ai caralho");
+    Thread.sleep(1000);
+    System.out.println("PEGA!");
+     }
+}
